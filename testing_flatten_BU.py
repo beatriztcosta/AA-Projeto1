Fixed image data normalization and cleaned code

testing_flatten_BU.py
@@ -12,48 +12,31 @@ from scipy.io import loadmat
 np.set_printoptions(threshold=sys.maxsize)
 
 size = 50
+
 def create_ml_data():
     ds = pd.read_csv('bee_dataset/relevant_bee_data.csv')
-    max_width = 0
-    max_height = 0
-    mean_width = 0
-    mean_height = 0
-    img_dict = {}
-    for idx,file in enumerate(ds['file']):
-        img_array = cv2.imread('bee_dataset/bee_imgs/'+file, cv2.IMREAD_GRAYSCALE)
-        h,w = img_array.shape
-        mean_height += h
-        mean_width += w
-        if h > max_height:
-            max_height = h
-        if w > max_width:
-            max_width = w
-        if h <= size and w <= size: 
-            vector_size = h*w
-            flat_img_array = img_array.flatten()
-            flat_img_array = np.reshape(flat_img_array,(1,vector_size))
-            flat_img_array = flat_img_array / 255
-            img_dict[idx] = {'features': flat_img_array, 'subspecie': ds['subspecies'][idx]}
-    
-    #mean_width = mean_width/i
-    #mean_height = mean_height/i
-    #print(mean_width)
-    #print(mean_height)
-
-    #vector_size = max_height*max_width
     vector_size = size*size
     features_array = []
     subspecies_array = []
-    for key, value in img_dict.items():
-        features = value['features']
-        h,w = features.shape
-        if h != max_height or w != max_width:
-            size_dif = vector_size-(h*w)
-            pad_features = np.pad(features, ((0,0),(0,size_dif)), mode='constant', constant_values=0)
-        pad_features = pad_features.flatten()
-        pad_features = ' '.join(map(str, pad_features))
-        features_array.append(pad_features)
-        subspecies_array.append(ds['subspecies'][key])
+    for idx,file in enumerate(ds['file']):
+        img_array = cv2.imread('bee_dataset/bee_imgs/'+file, cv2.IMREAD_GRAYSCALE)
+        h,w = img_array.shape
+        if h <= size and w <= size:
+            h_dif = size-h
+            zeros = np.zeros((h_dif,w), dtype=int)
+            img_array = np.vstack((img_array,zeros))
+            
+            w_dif = size-w
+            zeros = np.zeros((size,w_dif), dtype=int)
+            img_array = np.hstack((img_array,zeros))
+
+            flat_img_array = img_array.flatten()
+            flat_img_array = np.reshape(flat_img_array,(1,vector_size))
+            flat_img_array = flat_img_array / 255
+            flat_img_array = flat_img_array.flatten()
+            str_flat_img_array = ' '.join(map(str, flat_img_array))
+            features_array.append(str_flat_img_array)
+            subspecies_array.append(ds['subspecies'][idx])
         
     data = {'features': features_array, 'class': subspecies_array}
     df = pd.DataFrame(data=data, columns=['features', 'class'])
@@ -63,17 +46,14 @@ def test_csv_read():
     ds = pd.read_csv('bee_dataset/ml_data.csv')
     features=ds['features']
     y=ds['class']
-    #print(features.shape)
     n_examples = features.shape[0]
     feature_size = size*size
     X = np.zeros((n_examples,feature_size), float)
-    #np.fromstring(X.tostring(),a.dtype).reshape(a.shape)
     for idx,f in enumerate(features):
         arr_f = np.fromstring(f, dtype=float, sep=' ')
         arr_f = np.reshape(arr_f, (1,arr_f.shape[0]))
         X[idx,:] = np.copy(arr_f)
         
-    #X = np.copy(arr)
     print(X.shape)
 
     return X
@@ -86,6 +66,7 @@ def show_img(X):
 
     #cv2.imshow('img',a)
 
-#create_ml_data()
+
+create_ml_data()
 X = test_csv_read()
 show_img(X)